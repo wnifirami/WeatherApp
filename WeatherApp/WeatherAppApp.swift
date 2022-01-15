91e067c46e41ccf8cb45f04a863896dcd8cfc2b9
wnifirami <rami.ounifi@esprit.tn>
2022-01-15 19:04:31 +0100

    initial commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Rami Ounifi on 15/1/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
